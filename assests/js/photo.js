7a96088dfad3283ee195bcf1b9c69b928bbbf306
Arastuncode <arastunsa@code.edu.az>
2023-04-03 22:11:27 +0400

    Arm site

--- assests/js/photo.js
+++ assests/js/photo.js
@@ -1,23 +1,45 @@
-let  imgs = [ ['ŞUŞA', 'photo.jpg'], ['Heydər Əliyev','haliyev-2.jpg'], ['MƏCBURİ KÖÇKÜNLƏR','gachgin22-3-1.jpg'],
-              ['CƏBRAYIL RAYONU','cebrayil-xatire.jpeg'],['QUBADLI RAYONU', 'qubadli.jpg'],['FÜZULİ RAYONU','fuzuli-246.jpg'],
-              ['XANKƏNDİ 1963-CU İL, BAĞÇA UŞAQLARI GƏZİNTİDƏ','img-11004.jpeg'],['ŞUŞA, 1968', 'img-11005.jpeg'],['XAN QIZI XURŞİDBANU NATƏVANIN SARAY KOMPLEKSİ', 'xan.jpeg'],
-              ['ŞUŞA TƏBİƏTİ', 'susa.jpeg'],['KƏLBƏCƏRDƏ YERLƏŞƏN ŞAHMAT KLUBU 1993-CÜ İL', 'kelbecer.jpeg'],['HADRUT 1904-CÜ İL. ÜZEYİR HACIBƏYOV BU MƏKTƏBDƏ MÜƏLLİM İŞLƏYİRDİ','hadrut.jpeg'],
-              ['ŞUŞA. XARI BÜLBÜL FESTİVALI','xaribulbul.jpeg'],['XANKƏNDİ DÖVLƏT UNİVERSİTETİ','uni.jpeg'], ['ŞUŞA ƏTRAFI VƏ CIDIR DÜZÜ','jidir-duzu.jpg'], 
-              ['ƏSGƏRAN QALASI', 'askeran.jpg'], ['KƏLBƏCƏR', 'kalbacarda-korpu.jpg'], ['AĞDAM TARİXİ ŞƏKİLLƏR', 'agdam-1.jpg'],
-              ['ŞUŞA XATİRƏLƏRİM', 'img1.jpg'], ['CƏBRAYIL TARİXİ ŞƏKİLLƏR', 'cabrayil.jpg'],['KƏLBƏCƏR TARİXİ ŞƏKİLLƏR', 'slide66.jpg'], 
-              ['ŞUŞA 1959. HƏRBÇİ ƏLƏSGƏROV ƏNVƏR ÖZ EVİNİN EYVANINDA','scan-1-1.jpg'],['XALQ ARTİSTİ RASİM BALAYEV 35 İL SONRA AİLƏ ÜZVLƏRİ İLƏ BİRLİKDƏ EYNİ MƏKANDA, EYNİ TƏSVİRDƏ', 'rasim-balyev.jpeg'], 
-              ['QARABAĞ', 'fuzuli002.jpg'],['QARABAĞ','dji0351.jpg']
-            ];
-const imgList = document.querySelector('.imglist')
+let imgs = [
+  ["ŞUŞA", "photo.jpg"],
+  ["Heydər Əliyev", "haliyev-2.jpg"],
+  ["MƏCBURİ KÖÇKÜNLƏR", "gachgin22-3-1.jpg"],
+  ["CƏBRAYIL RAYONU", "cebrayil-xatire.jpeg"],
+  ["QUBADLI RAYONU", "qubadli.jpg"],
+  ["FÜZULİ RAYONU", "fuzuli-246.jpg"],
+  ["XANKƏNDİ 1963-CU İL, BAĞÇA UŞAQLARI GƏZİNTİDƏ", "img-11004.jpeg"],
+  ["ŞUŞA, 1968", "img-11005.jpeg"],
+  ["XAN QIZI XURŞİDBANU NATƏVANIN SARAY KOMPLEKSİ", "xan.jpeg"],
+  ["ŞUŞA TƏBİƏTİ", "susa.jpeg"],
+  ["KƏLBƏCƏRDƏ YERLƏŞƏN ŞAHMAT KLUBU 1993-CÜ İL", "kelbecer.jpeg"],
+  [
+    "HADRUT 1904-CÜ İL. ÜZEYİR HACIBƏYOV BU MƏKTƏBDƏ MÜƏLLİM İŞLƏYİRDİ",
+    "hadrut.jpeg",
+  ],
+  ["ŞUŞA. XARI BÜLBÜL FESTİVALI", "xaribulbul.jpeg"],
+  ["XANKƏNDİ DÖVLƏT UNİVERSİTETİ", "uni.jpeg"],
+  ["ŞUŞA ƏTRAFI VƏ CIDIR DÜZÜ", "jidir-duzu.jpg"],
+  ["ƏSGƏRAN QALASI", "askeran.jpg"],
+  ["KƏLBƏCƏR", "kalbacarda-korpu.jpg"],
+  ["AĞDAM TARİXİ ŞƏKİLLƏR", "agdam-1.jpg"],
+  ["ŞUŞA XATİRƏLƏRİM", "img1.jpg"],
+  ["CƏBRAYIL TARİXİ ŞƏKİLLƏR", "cabrayil.jpg"],
+  ["KƏLBƏCƏR TARİXİ ŞƏKİLLƏR", "slide66.jpg"],
+  ["ŞUŞA 1959. HƏRBÇİ ƏLƏSGƏROV ƏNVƏR ÖZ EVİNİN EYVANINDA", "scan-1-1.jpg"],
+  [
+    "XALQ ARTİSTİ RASİM BALAYEV 35 İL SONRA AİLƏ ÜZVLƏRİ İLƏ BİRLİKDƏ EYNİ MƏKANDA, EYNİ TƏSVİRDƏ",
+    "rasim-balyev.jpeg",
+  ],
+  ["QARABAĞ", "fuzuli002.jpg"],
+  ["QARABAĞ", "dji0351.jpg"],
+];
+const imgList = document.querySelector(".imglist");
 
 for (let i = 0; i < imgs.length; i++) {
-    imgList.innerHTML += `
+  imgList.innerHTML += `
     <a href="./assests/img/${imgs[i][1]}" data-fancybox="group"
         data-caption="${imgs[i][0]}">
         <img src="./assests/img/${imgs[i][1]}" />
     </a>
-    `
-    
+    `;
 }
 
 // $("[data-fancybox]").fancybox({
