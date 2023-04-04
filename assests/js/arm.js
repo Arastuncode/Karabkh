d346288ce99f97948818a46e618d30b788faa410
Arastuncode <arastunsa@code.edu.az>
2023-04-04 16:08:41 +0400

    Arm

---
+++ assests/js/arm.js
@@ -0,0 +1,7 @@
+$(".modal-toggle").click(function () {
+    $('.modal').fadeToggle('fast');
+    console.log("salam");
+});  
+$(".close").click(function () {
+    $('.modal').fadeToggle('fast');
+});  
