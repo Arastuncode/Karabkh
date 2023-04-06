db531c0a1220890096c3c3ed6cb002188e764df6
Arastuncode <arastunsa@code.edu.az>
2023-04-06 23:39:41 +0400

    Modals

--- assests/js/president.js
+++ assests/js/president.js
@@ -1,5 +1,11 @@
+let h5 = $("#h5")
+let iframe = $("iframe")
 $(".modal-toggle").click(function () {
     $('.modal').fadeToggle('fast');
+    h5.html($(this).next('p').html());
+    let src = $(this).closest("div").find("img").att()
+    console.log(src);
+    // iframe.attr("src",src)    
 });  
 $(".close").click(function () {
     $('.modal').fadeToggle('fast');
