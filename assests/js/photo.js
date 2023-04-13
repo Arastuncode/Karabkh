beb112712be1536dcf24759492e8fa7340502541
Arastuncode <arastunsa@code.edu.az>
2023-04-13 17:53:45 +0400

    Image gallery

--- assests/js/photo.js
+++ assests/js/photo.js
@@ -30,24 +30,43 @@ let kod = "";
 for (let i = 0; i < imgs.length; i++) {
   kod +=
    `
+    <div class="box">
       <a href="./assests/img/photo/${imgs[i][1]}" data-fancybox="group"
         data-caption="${imgs[i][0]}">
         <img src="./assests/img/photo/${imgs[i][1]}" />
       </a>
+    </div>
     `;
 }
 imgList.html(kod);
 
-// $("[data-fancybox]").fancybox({
-//     buttons: ["close"],
-//     wheel: false,
-//     transitionEffect: "slide",
-//     thumbs: false,
-//     hash: true,
-//     loop: true,
-//     keyboard: true,
-//     toolbar: true,
-//     animationEffect: true,
-//     arrows: true,
-//     clickContent: true,
-// });
+let boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+  box.addEventListener("mousemove", (e) => {
+    let halfWidth = box.clientWidth / 2,
+    halfHeight = box.clientHeight / 2,
+    mouseX = halfWidth + box.offsetLeft - e.pageX,
+    mouseY = halfHeight + box.offsetTop - e.pageY,
+    maxDeg = 10,
+    image = box.querySelector("img");
+    let degX = (mouseY / halfHeight) * -maxDeg + "deg";
+    let degY = (mouseX / halfWidth) * maxDeg + "deg";
+    box.setAttribute(
+      "style",
+      `transform: perspective(512px) rotateX(${degX}) rotateY(${degY});`
+    );
+
+    image.setAttribute(
+      "style",
+      `object-position: ${
+        (box.offsetLeft + box.clientWidth - e.pageX) / -8 + 20
+      }px ${(box.offsetTop + box.clientHeight - e.pageY) / -8 + 20}px`
+    );
+  });
+
+  box.addEventListener("mouseout", () => {
+    box.removeAttribute("style");
+    box.querySelector("img").removeAttribute("style");
+  });
+});
